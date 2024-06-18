d879d3941d6547dcd1c0959b844ff9f57248d78e
Daniel Williams <williamsoligodaniel@gmail.com>
2024-06-18 17:22:00 +0100

    add state to header

--- NextGenAfrica/src/components/Header.jsx
+++ NextGenAfrica/src/components/Header.jsx
@@ -1,16 +1,23 @@
-import React from 'react';
+import React, { useState }  from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
+	const [isOpen, setIsOpen] = useState(false);
+
+	const toggleMenu = () => {
+		setIsOpen(!isOpen)
+	}
 	  return (
-		      <header className="">
-		        <h1 className="">NextGen Africa</h1>
-		        <nav className="">
-		          <a href="#linux" className="text-white hover:text-gray-300">Linux</a>
-		          <a href="#mongodb" className="text-white hover:text-gray-300">MongoDB</a>
-		          <a href="#frontend" className="text-white hover:text-gray-300">Frontend</a>
-		          <a href="#backend" className="text-white hover:text-gray-300">Backend</a>
+		      <header className="bg-blue-500 p-4 flex justify-between items-center ">
+		        <h1 className="text-white text-3xl">NextGen Africa</h1>
+		        <nav className={`flex-col md:flex-row md:flex space-x-4 ${isOpen ? 'flex' : 'hidden'} md:flex`}>
+		          <Link to="/" className="text-white hover:text-gray-300">Home</Link>
+		          <Link to="/overview" className="text-white hover:text-gray-300">Overview</Link>
+		          <Link to="/about" className="text-white hover:text-gray-300">About</Link>
 		        </nav>
-		        <button className="md:hidden text-white">Menu</button>
+		        <button onClick={toggleMenu} className="md:hidden text-white">
+		          {isOpen ? 'close' : 'Menu'}
+		        </button>
 		      </header>
 		    );
 }
