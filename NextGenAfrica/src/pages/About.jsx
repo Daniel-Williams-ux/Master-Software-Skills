abf12bd5d0a8698817cfa2e448b459672e74ff10
Daniel Williams <williamsoligodaniel@gmail.com>
2024-06-18 14:55:10 +0100

    create page components

---
+++ NextGenAfrica/src/pages/About.jsx
@@ -0,0 +1,13 @@
+mport React from 'react';
+
+function About() {
+	  return (
+		      <div>
+		        <h2>About</h2>
+		        <p>About NextGen Africa.</p>
+		      </div>
+		    );
+}
+
+export default About;
+
---
+++ NextGenAfrica/src/pages/Overview.jsx
@@ -0,0 +1,13 @@
+mport React from 'react';
+
+function Overview() {
+	  return (
+		      <div>
+		        <h2>Overview</h2>
+		        <p>Learn about our programs and offerings.</p>
+		      </div>
+		    );
+}
+
+export default Overview;
+
