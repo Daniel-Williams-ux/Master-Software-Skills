c541a51f17085c0b5280b03a9a4d513c851ef320
Daniel Williams <williamsoligodaniel@gmail.com>
2024-06-18 14:50:08 +0100

    define routes

--- NextGenAfrica/src/App.jsx
+++ NextGenAfrica/src/App.jsx
@@ -1,13 +1,21 @@
 import React from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import Header from './components/Header';
+import Home from './pages/Home'; 
+import Overview from './pages/Overview';
+import About from './pages/About'; 
 
 function App() {
 	  return (
 		      <div className="App">
 		        <Header />
 		        <main>
-		          {/* Sections will be rendered here */}
+		          <Routes>
+		            <Route path="/" element={<Home />} />
+		            <Route path="/overview" element={<Overview />} />
+		            <Route path="/about" elememt={<About />} />
+		          </Routes>
 		        </main>
 		        <footer className="bg-gray-800 text-white p-4">
 		          <p>&copy; 2024 NextGen Africa. All rights reserved.</p>
