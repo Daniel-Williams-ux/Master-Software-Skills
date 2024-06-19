1d7b017701436aca155de060a7e082e4396d8c77
Daniel Williams <williamsoligodaniel@gmail.com>
2024-06-19 09:00:59 +0100

    updated logo

--- NextGenAfrica/src/components/Header.jsx
+++ NextGenAfrica/src/components/Header.jsx
@@ -9,15 +9,15 @@ function Header() {
 		setIsOpen(!isOpen)
 	}
 	  return (
-		      <header className="bg-white py-4 flex justify-between items-center top-0 left-0 w-full z-50 shadow-md fixed">
-		        <img src={logo} alt="NextGen Africa" className="w-24" />
+		      <header className="bg-black py-4 flex justify-between items-center top-0 left-0 w-full z-50 shadow-md fixed">
+		        <img src={logo} alt="NextGen Africa" className="w-34 h-10 sm:h-10  contrast-200" />
 		        <nav className={`flex-col md:flex-row md:flex space-x-4 ${isOpen ? 'flex' : 'hidden'} md:flex`}>
-		          <Link to="/" className="text-black hover:text-gray-300">Home</Link>
-		          <Link to="/overview" className="text-black hover:text-gray-300">Overview</Link>
-		          <Link to="/about" className="text-black hover:text-gray-300">About</Link>
+		          <Link to="/" className="text-white hover:text-gray-300">Home</Link>
+		          <Link to="/overview" className="text-white hover:text-gray-300">Overview</Link>
+		          <Link to="/about" className="text-white hover:text-gray-300">About</Link>
 		        </nav>
 		        <button onClick={toggleMenu} className="md:hidden text-white">
-		          {isOpen ? 'close' : 'Menu'}
+		          {isOpen ? 'close' : ' Menu'}
 		        </button>
 		      </header>
 		    );
